3c589792349c7caa049e056b640ce8dd112572b1
Camilo Saavedra <ci.saavedras@gmail.com>
2023-11-29 00:09:20 -0300

    Initial Commit

---
+++ QR Validation/QR_ValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_ValidationApp.swift
+//  QR Validation
+//
+//  Created by Camilo Saavedra Salas on 29-11-23.
+//
+
+import SwiftUI
+
+@main
+struct QR_ValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
